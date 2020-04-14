3f7d09a4bf1d8c60543c733ee07a2ee8ebebb163
rickhodder <rickhodder@msn.com>
2020-04-13 22:25:11 -0400

    Get rid of List<Player> - makes logic clearer

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -5,7 +5,6 @@ namespace Tennis
 {
     class Player
     {
-
         private static string[] PointNames = new string[]{
             "Love",
             "Fifteen",
@@ -17,8 +16,10 @@ namespace Tennis
         {
             Name = name;
         }
+     
         public string Name { get; }
         private int _points;
+     
         public int Points
         {
             get { return _points; }
@@ -37,35 +38,41 @@ namespace Tennis
 
     class TennisGame1 : ITennisGame
     {
-        private List<Player> _players = new List<Player>();
+        Player _player1;
+        Player _player2;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            _players.Add(new Player(player1Name));
-            _players.Add(new Player(player2Name));
+            _player1 = new Player(player1Name);
+            _player2 = new Player(player2Name);
         }
 
         public void WonPoint(string playerName)
         {
-            _players.First(p => p.Name == playerName).WonPoint();
+            if(_player1.Name==playerName)
+                {
+                    _player1.WonPoint();
+                }
+                else
+                {
+                    _player2.WonPoint();
+                }
         }
 
         public string GetScore()
         {
             string score = "";
-            var player1 = _players[0];
-            var player2 = _players[1];
 
-            if (player1.Points == player2.Points)
+            if (_player1.Points == _player2.Points)
             {
-                if (player1.Points > 2)
+                if (_player1.Points > 2)
                     score = "Deuce";
                 else
-                    score = player1.GetScore() + "-All";
+                    score = _player1.GetScore() + "-All";
             }
-            else if (player1.Points >= 4 || player2.Points >= 4)
+            else if (_player1.Points >= 4 || _player2.Points >= 4)
             {
-                var pointDifference = player1.Points - player2.Points;
+                var pointDifference = _player1.Points - _player2.Points;
                 if (pointDifference == 1) score = "Advantage player1";
                 else if (pointDifference == -1) score = "Advantage player2";
                 else if (pointDifference >= 2) score = "Win for player1";
@@ -73,7 +80,7 @@ namespace Tennis
             }
             else
             {
-                score = player1.GetScore() + "-" + player2.GetScore();
+                score = _player1.GetScore() + "-" + _player2.GetScore();
             }
             return score;
         }
